53788d397b6aeb40ee05c6e466b1549453c7fcf9
otreblan <otreblain@gmail.com>
2020-06-13 18:51:38 -0500

    Merge https://github.com/otreblan/hello

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+// Observer
+// Copyright © 2019-2020 otreblan
+//
+// observer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// observer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with observer.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello\n";
+	return 0;
+}
